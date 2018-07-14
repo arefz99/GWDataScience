0446743bf172d4d40127aa4ba62163b490dddd26
Aref Erfani <arefz99@users.noreply.github.com>
2018-07-14 13:59:36 -0400

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# Twitter API Keys
-#Aref info
-consumer_key = "D1AnnXeZYxZRwvzOylguOYVGY"
-consumer_secret = "x3z5ly2MvViXXRvM2ypac2zWn7ESYKxPovFY8rK344wXmKEAyD"
-access_token = "1013060892580286464-aaRUc6MKQE3AS1mCY636ork3t6veDT"
-access_token_secret = "T2nO13meguQdqETf6aw9fQyaGDP99zKa88NoKvzYmCYqA"
